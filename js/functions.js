2e3a26e4179fd17ea93b25a2288688d0eafc02b3
Marcos Troncoso <mtroncoso@prosoftnearshore.com>
2013-08-29 18:07:01 -0400

    added js

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+function pantallacompleta (pagina)
+{
+var opciones=("toolbar=yes,location=yes, directories=yes, status=yes, menubar=yes ,scrollbars=yes, resizable=yes, width="+screen.width+", height="+screen.height+"");
+window.open(pagina,"",opciones);
+}
